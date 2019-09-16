e68ad98f0b2075ab87de68e95ce220e986f60406
admin <admin@admins-MacBook-Pro.local>
2019-09-15 20:59:17 -0700

    Updates

---
+++ precache-manifest.f1f7b9b1b34fc2c4acbf2600ea8238bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "554b6b7167da75ee981f183225362fac",
+    "url": "/marvelguide/index.html"
+  },
+  {
+    "revision": "9867b7595de854de32c8",
+    "url": "/marvelguide/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c1655296099dba4dbc43",
+    "url": "/marvelguide/static/js/2.4736327b.chunk.js"
+  },
+  {
+    "revision": "9867b7595de854de32c8",
+    "url": "/marvelguide/static/js/main.7f7ec3c2.chunk.js"
+  },
+  {
+    "revision": "848c742001e65b108fad",
+    "url": "/marvelguide/static/js/runtime~main.6f39636d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/marvelguide/static/media/logo.5d5d9eef.svg"
+  }
+]);
