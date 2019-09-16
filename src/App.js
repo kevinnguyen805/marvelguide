cdd8b71445ad9ff7b1ce0561c337c5d84a4bd636
admin <admin@admins-MacBook-Pro.local>
2019-09-15 21:05:39 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+Hello        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
